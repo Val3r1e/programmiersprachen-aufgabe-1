4074f3919f9cc4f4abf142a60e9d054902db6359
Valerie Lemuth <valerie@eduroam-049-185.scc.uni-weimar.de>
2017-04-12 16:03:42 +0200

    First commit

---
+++ source/helloworld.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+void aufgabebla()
+{
+  for (long int num = 0; num < 400000; ++num)
+  {
+    for (int nam = 1; nam <= 20; ++nam){
+      if ()
+    }
+  }
+}
+
+int main()
+{
+  std::cout << "Hello, World!\n";
+  return 0;
+}
